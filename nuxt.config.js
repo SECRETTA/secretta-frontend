52eea6af346650f1c6f9074797efceb9802b578f
Valentina Nakayama <v_nakayama@outlook.com>
2021-05-25 10:58:33 -0300

    dark_mode com ícone implementado, a burra sou eu

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,7 +69,17 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // '@nuxtjs/color-mode',
+    ['@nuxtjs/color-mode', {
+      colorMode: {
+        preference: 'system', // default value of $colorMode.preference
+        fallback: 'light', // fallback value if not system preference found
+        hid: 'nuxt-color-mode-script',
+        globalName: '__NUXT_COLOR_MODE__',
+        componentName: 'ColorScheme',
+        classPrefix: '',
+        classSuffix: '-mode',
+        storageKey: 'nuxt-color-mode'
+    }}],    
     'bootstrap-vue/nuxt',
     ['nuxt-fontawesome', {
       component: 'fa',
