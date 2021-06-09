490c29ba71a91fdf5febb76f635338f2c815fe17
LuizGiserman <luizgiserman@poli.ufrj.br>
2021-06-08 21:14:00 -0300

    Now fetches tasks from API

--- components/dashboard/AppointTable.vue
+++ components/dashboard/AppointTable.vue
@@ -15,15 +15,15 @@
     ></b-table>
 
     <div class="pagination">
-        <b-pagination 
-            v-model="currentPage" 
-            per-page="7" 
-            pills 
+        <b-pagination
+            v-model="currentPage"
+            per-page="7"
+            pills
             :total-rows="rows"
             size="sm">
         </b-pagination>
     </div>
-    
+
   </div>
 </template>
 
@@ -41,7 +41,7 @@
           { key: 'task_time', sortable: false, label: 'Horário' }
         ],
         items: [
-          {customer_name: 'João Adamastor', task_date:'27/5/2021', task_time:'9h'}
+          // {customer_name: 'João Adamastor', task_date:'27/5/2021', task_time:'9h'}
         ]
       }
     },
@@ -50,8 +50,32 @@
         return this.items.length
       }
     },
+    created : function () {
+      this.fetchTasks();
+    },
     methods: {
-    }
+      fetchTasks ()
+      {
+        console.log('start-fetch')
+        fetch('http://localhost:3001/api/task/userid/1'/*,
+          {
+            headers: { 'Content-Type': 'application/json'},
+            method: 'get',
+          }*/)
+          .then(res => res.json())
+          .then(obj => {
+            console.log(obj);
+            console.log('len', obj['tasks'].length);
+            for (let i=0; i < obj['tasks'].length; i++){
+              let startDate = new Date(obj['tasks'][i]['start']);
+              let taskDate = String(startDate.getDate()) + '/' + String(startDate.getMonth() + 1) + '/' + String(startDate.getYear() - 100);
+              console.log(taskDate);
+              let newItem = {customer_name: obj['tasks'][i]['customerName'], task_date: taskDate, task_time:'9h'};
+              this.items.push(newItem);
+            }
+          })
+      }
+    },
   }
 </script>
 
